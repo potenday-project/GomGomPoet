04a88dadc071a1a6d5e537b52c48787e482795d7
jominuook <jominuook@gmail.com>
2023-12-14 16:29:01 +0900

    Cors 허용

--- GomGomPoet-Back/index.js
+++ GomGomPoet-Back/index.js
@@ -1,8 +1,10 @@
 const https = require('https');
 const { HTTPS, CLOVA, PORT } = require('./constants');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.static('web-build'));
 
 const getStream = async (res, body) => {
