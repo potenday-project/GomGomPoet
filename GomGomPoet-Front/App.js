61b9e40b169c163db9954b2f6fd42de0e088b5f6
jominuook <jominuook@gmail.com>
2023-12-14 11:40:36 +0900

    로그인 페이지 삭제

--- GomGomPoet-Front/App.js
+++ GomGomPoet-Front/App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './src/Login';
 import Confide from './src/Confide';
 import Comfort from './src/Comfort';
 
@@ -8,8 +7,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
-                <Stack.Screen name='Login' component={Login} options={{ title: '곰곰시인' }}/>
+            <Stack.Navigator initialRouteName='Confide' screenOptions={{ headerShown: false }}>
                 <Stack.Screen name='Confide' component={Confide} options={{ title: '곰곰시인' }}/>
                 <Stack.Screen name='Comfort' component={Comfort} options={{ title: '곰곰시인' }}/>
             </Stack.Navigator>
