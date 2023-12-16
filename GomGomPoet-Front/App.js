1c57344bbbf4dc60185097f2a1948297cb4bc6d5
jominuook <jominuook@gmail.com>
2023-12-17 00:47:07 +0900

    Loading custom fonts

--- GomGomPoet-Front/App.js
+++ GomGomPoet-Front/App.js
@@ -1,11 +1,29 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Font from 'expo-font';
+import React, { useState, useEffect } from 'react';
 import Confide from "./src/Confide";
 import Comfort from "./src/Comfort";
 import History from "./src/History";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+  let [isReady, setReady] = useState(false);
+
+  const fetchFonts = async () => {
+    await Font.loadAsync({
+        SKYBORI: require('./assets/fonts/SKYBORI.ttf'),
+        YeongdeokSnowCrab: require('./assets/fonts/YeongdeokSnowCrab.ttf')
+    });
+    setReady(true);
+  }
+
+  useEffect(fetchFonts, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
